086a7260b9838ede905a9118f961bf5ed95eb929
dizhang26 <47244157+dizhang26@users.noreply.github.com>
2019-10-11 13:59:06 -0700

    Update ArrayStack.java

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,34 +1,35 @@
 import java.util.Arrays;
-public class ArrayStack<T> implements Stack<T>{
+public class ArrayStack<T> implements Stack<T>{ //implements interface Stack and build functions
 	private T[] array = (T[]) new Object[10];
-	private int top = -1;
+	private int top = -1; //set the the top of the array to be -1
 
-	public void growArray(){
+	public void growArray(){ //grow array function, to double the size of the array.
 		T[] arr = (T[]) new Object[2 * array.length];
 		for (int i = 0; i < array.length; i++)
 			arr[i] = array[i];
-        array = arr;
+        	array = arr;
 	}
-	public T peek(){
+	public T peek(){ //check if the array is empty, if is it return null.
 		if (empty()) 
 			return null;
 		return array[top];
 	}
 	public T pop(){
-		if (empty()) 
+		if (empty()) //check if the array is empty, if is it return null.
 			return null;
-		T t = array[top];
-		top--;
-		final var t1 = t;
+		T t = array[top];//store the last element in the array and return it later
+		top--; //decrement the index of top.
+		final var t1 = t; 
 		return t1;
 	}
 	public void push(T item){
-		if (top == array.length - 1) growArray();
-		top++;
+		if (top == array.length - 1) //check if the array if full, if it is call growArray function
+			growArray();
+		top++;//increment top
 		array[top] = item;
 	}
 
-	public boolean empty(){
+	public boolean empty(){//when top = -1, the array is empty.
 		return top == -1;
 	}	
 }
